944f7542b0b7219822dfae738eb9fc0048870f4a
Mmanusia <mmanusia987@gmail.com>
2026-05-06 10:58:49 +0700

    plss succes

---
+++ lib/nael_main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(PenjajakanApps());
+}
+
+class PenjajakanApps extends StatefulWidget {
+  const PenjajakanApps({super.key});
+
+  @override
+  State<PenjajakanApps> createState() => _PenjajakanAppsState();
+}
+
+class _PenjajakanAppsState extends State<PenjajakanApps> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
