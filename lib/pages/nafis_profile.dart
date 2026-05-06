b8bdb54f2892fbdcba9abb564704fba3c91e1868
Mmanusia <mmanusia987@gmail.com>
2026-05-06 12:34:51 +0700

    plss succes

---
+++ lib/pages/nafis_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NafisProfile extends StatefulWidget {
+  const NafisProfile({super.key});
+
+  @override
+  State<NafisProfile> createState() => _NafisProfileState();
+}
+
+class _NafisProfileState extends State<NafisProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
