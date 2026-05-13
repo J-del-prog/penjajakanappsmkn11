f1ce323446078870cab13b422e024206577a70eb
tugasakhir <nendima7@gmail.com>
2026-05-13 14:00:57 +0700

    tekarsok akarsu

--- lib/services/nael_feb_service.dart
+++ lib/services/nael_feb_service.dart
@@ -1,12 +1,12 @@
 import 'package:penjajakanappsmkn11/model/nael_feb.dart';
 
 List <Naelfeb> murid2 = [
-Naelfeb(image2: "assets/image/img_2.png",nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 0),
-  Naelfeb(image2: "assets/image/img_2.png",nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 1),
-  Naelfeb(image2: "assets/image/img_2.png",nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 2),
-  Naelfeb(image2: "assets/image/img_2.png",nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 3),
-  Naelfeb(image2: "assets/image/img_2.png",nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 4),
-
+Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "10/10", id2: 0, image2: 'assets/image/img_2.png'),
+  Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "6/10", id2: 1, image2: 'assets/image/img_2.png'),
+  Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "8/10", id2: 2, image2: 'assets/image/img_2.png'),
+  Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "5/10", id2: 3, image2: 'assets/image/img_2.png'),
+  Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "1/10", id2: 4, image2: 'assets/image/img_2.png'),
+  Naelfeb(nama2: "IRHAM BURGER", kelengkapan2: "6/10", id2: 5, image2: 'assets/image/img_2.png'),
 
 
 ];
--- lib/services/nael_mei_service.dart
+++ lib/services/nael_mei_service.dart
@@ -5,10 +5,9 @@ import 'package:penjajakanappsmkn11/model/nael_mei.dart';
 import '../model/Nael_jan.dart';
 
 List <Naelmei> murid5 = [
-  Naelmei(image5: "assets/image/img_2.png",nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 0),
-  Naelmei(image5: "assets/image/img_2.png",nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 1),
-  Naelmei(image5: "assets/image/img_2.png",nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 2),
-  Naelmei(image5: "assets/image/img_2.png",nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 3),
-  Naelmei(image5: "assets/image/img_2.png",nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 4),
-
+  Naelmei(nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 0, image5: 'assets/image/img_2.png'),
+  Naelmei(nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 1, image5: 'assets/image/img_2.png'),
+  Naelmei(nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 2, image5: 'assets/image/img_2.png'),
+  Naelmei(nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 3, image5: 'assets/image/img_2.png'),
+  Naelmei(nama5: "IRHAM Burger", kelengkapan5: "5/10", id5: 4, image5: 'assets/image/img_2.png'),
 ];
