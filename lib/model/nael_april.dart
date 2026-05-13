4d39fa6c6d6d2d964193e6cd5838741db64b06e9
tugasakhir <nendima7@gmail.com>
2026-05-13 15:20:13 +0700

    tekarsok akarsu

--- lib/model/nael_april.dart
+++ lib/model/nael_april.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+
 class Naelapri {
   final String image4;
   final String nama4;
@@ -6,5 +7,21 @@ class Naelapri {
   final int id4;
   bool mark4 = false;
 
-  Naelapri({required this.image4, required this.nama4, required this.kelengkapan4, required this.id4});
+  Naelapri({
+    required this.image4,
+    required this.nama4,
+    required this.kelengkapan4,
+    required this.id4,
+  });
 }
+
+//
+// class Teacher {
+//   final List<Naelapri> mapel;
+//
+//   Teacher({required this.mapel});
+// }
+//
+// List<Teacher> t = [
+//   Teacher(mapel: [Naelapri(image4: image4, nama4: nama4, kelengkapan4: kelengkapan4, id4: id4)])
+//]
